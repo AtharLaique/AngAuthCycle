08ef77675e966c0252e2138bdec41c9a08dfe462
Athar Laique <atharlaique15@gmail.com>
2020-02-24 16:02:14 +0500

    PHASE 1  [ HttpClient]

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,12 +1,14 @@
 
 //This service is  responcible for user login and user signup and mnaging user data 
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 //If we use providedIn, the injectable is registered as a provider of the Module without 
 //adding it to the providers of the module.
 @Injectable({providedIn:'root'})
 export class AuthService {
-    constructor(){}
+    constructor( private http:HttpClient){}
     //Signup
     signup(){}
      //Login
