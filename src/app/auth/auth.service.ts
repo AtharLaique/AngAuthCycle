0d86abb79410af86860f4bf684659e674c4edf4a
Athar Laique <atharlaique15@gmail.com>
2020-02-24 16:00:26 +0500

    PHASE 1 [ register service ]

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,10 @@
+
 //This service is  responcible for user login and user signup and mnaging user data 
+import { Injectable } from '@angular/core';
+
+//If we use providedIn, the injectable is registered as a provider of the Module without 
+//adding it to the providers of the module.
+@Injectable({providedIn:'root'})
 export class AuthService {
     constructor(){}
     //Signup
