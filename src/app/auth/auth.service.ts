3001c037bb9cb30a94dd9b02dd03feee52ccb3c6
Athar Laique <atharlaique15@gmail.com>
2020-02-24 15:56:42 +0500

    PHASE 1 [ class and methods for auth service]

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,1 +1,8 @@
 //This service is  responcible for user login and user signup and mnaging user data 
+export class AuthService {
+    constructor(){}
+    //Signup
+    signup(){}
+     //Login
+    login(){}
+} 
