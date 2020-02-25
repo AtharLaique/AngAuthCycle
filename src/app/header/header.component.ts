deca25cb97600030046126534e2eaf1eed66ebb7
Athar Laique <atharlaique15@gmail.com>
2020-02-25 12:24:22 +0500

    PHASE 4 [Subscribe the auth service]

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
-
+export class HeaderComponent implements OnInit {
+  constructor(private dataStorageService: DataStorageService ,private auth:AuthService) {}
+   ngOnInit(){
+     this.auth.user.subscribe(user=>{
+       console.log(user)
+     })
+   }
   onSaveData() {
     this.dataStorageService.storeRecipes();
   }
