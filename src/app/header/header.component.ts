5df5c17349c1ca813d3b91d8795c8e1faa640fea
Athar Laique <atharlaique15@gmail.com>
2020-02-25 12:28:42 +0500

    PHASE 4 [ set isAuthuntecated user flag]

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 
@@ -8,10 +7,12 @@ import { AuthService } from '../auth/auth.service';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+  isAuthentecated=false;
   constructor(private dataStorageService: DataStorageService ,private auth:AuthService) {}
    ngOnInit(){
      this.auth.user.subscribe(user=>{
-       console.log(user)
+       this.isAuthentecated=!!user
+       console.log( this.isAuthentecated)
      })
    }
   onSaveData() {
