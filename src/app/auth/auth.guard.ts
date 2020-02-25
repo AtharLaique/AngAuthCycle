bbb870ae0170ec4ee635f60f2aaa04cf7a75823f
Athar Laique <atharlaique15@gmail.com>
2020-02-25 18:31:37 +0500

    PHASE 8 [ auth guard service ]

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,25 @@
+import { 
+    CanActivate ,
+    ActivatedRouteSnapshot ,
+    RouterStateSnapshot ,
+    Router} 
+from "@angular/router";
+
+import {Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+import {AuthService} from './auth.service';
+import {Injectable} from '@angular/core'
+//Step 4.1 inject the service
+@Injectable()
+export class AuthGuard implements CanActivate{
+    constructor(private auth:AuthService,private router:Router){}
+    canActivate(
+        route:ActivatedRouteSnapshot , 
+        state:RouterStateSnapshot):
+        Observable<boolean>|Promise<boolean>|boolean
+    {
+        return this.auth.user.pipe(map(user=>{
+            return !!user;
+        }))
+    }
+}
